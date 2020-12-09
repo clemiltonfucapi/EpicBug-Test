c780600eb6aa2d7d37a238bafc12251fee31af6a
clemiltonfucapi <clemilton.fucapi@gmail.com>
2020-12-09 14:41:39 -0400

    toast anderson123

--- app/src/main/java/com/anderson/epicbug/MainActivity.java
+++ app/src/main/java/com/anderson/epicbug/MainActivity.java
@@ -1,7 +1,7 @@
 package com.anderson.epicbug;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androix.widgets.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeTest(getApplicationContext(),"Anderson123",Toast.LENGHT_SHORT).show();
     }
 }
